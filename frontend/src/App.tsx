f5e7750b525890c60bb2268f08a64a207015dda2
tkaixiang <tkaixiang@gmail.com>
2025-01-18 16:18:34 +0800

    hawk tuah

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import NameDialog from "./components/NameDialog";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [showDialog, setShowDialog] = useState(true);
 
   return (
-    <div className="w-screen h-screen overflow-hidden">
+    <div className="w-screen h-screen overflow-hidden p-4">
       {/* Header */}
-      <div className="h-18 w-full">
+      <div className="flex h-18 w-full justify-between">
         <div className="header-title">CRASH OUT!</div>
-        <button onClick={() => setShowDialog(true)}>Change username</button>
+        <NameDialog setOpen={setShowDialog} open={showDialog} />
       </div>
       {/* Main Content */}
       <div className="flex h-full w-full mt-8">
@@ -27,8 +28,11 @@ function App() {
         </div>
 
         {/* Finals Results and Sleep */}
-        <div className="w-1/4">
-          <div className="finals-results">
+        <div className="w-1/4 flex flex-col justify-center">
+          <div className="h-1/4 flex justify-center">
+            <Button>CRASH OUT</Button>
+          </div>
+          <div className="h-3/4">
             <h3>FINALS RESULTS</h3>
             <ul>
               <li>CS1101S - B</li>
@@ -36,11 +40,8 @@ function App() {
               <li>CS1101S</li>
             </ul>
           </div>
-          <button className="sleep-button">CRASH OUT</button>
         </div>
       </div>
-
-      <NameDialog setOpen={setShowDialog} open={showDialog} />
     </div>
   );
 }
